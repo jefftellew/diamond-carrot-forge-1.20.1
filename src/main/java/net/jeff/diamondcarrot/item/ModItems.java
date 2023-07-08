feb949ad5c43dc80f17df6adfebef18bb94388f5
Jeff Tellew <jefftellew@gmail.com>
2023-07-08 14:38:01 -0700

    Add food props

--- src/main/java/net/jeff/diamondcarrot/item/ModItems.java
+++ src/main/java/net/jeff/diamondcarrot/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jeff.diamondcarrot.item;
 
 import net.jeff.diamondcarrot.DiamondCarrot;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,9 +12,18 @@ public class ModItems {
     // Holds a list of the items that will make up the mod -- in this case, Diamond Carrots
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DiamondCarrot.MOD_ID);
 
-    public static final RegistryObject<Item> DIAMOND_CARROT = ITEMS.register("diamond_carrot", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_CARROT = ITEMS.register("diamond_carrot", () -> new Item(new Item.Properties().food(Foods.DIAMOND_CARROT_PROPERTIES)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+    }
+
+    public static class Foods {
+        public static final FoodProperties DIAMOND_CARROT_PROPERTIES = new FoodProperties.Builder()
+                .nutrition(20)
+                .saturationMod(20.0f)
+                .alwaysEat()
+                .fast()
+                .build();
     }
 }
